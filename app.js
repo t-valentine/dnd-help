fba6cccf213b5a7eb181530a1bc47b69e56801e9
Tobias Wallace <twallace@catalyte.io>
2022-08-08 10:26:06 -0500

    Add functionality for skills menu

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-const roll = () => {
+const skillsHelp = () => {
+  var selection = document.getElementById('skills').value;
+  var result = document.getElementById('skill-p');
+  result.innerHTML = "";
+  switch (selection) {
+    case "acrobatics" || "sleight" || "stealth":
+      result.append("Use Dexterity");
+      break;
+    case "arcana" || "history" || "invest" || "nature" || "religion":
+      result.append("Use Intelligence");
+      break;
+    case "animal" || "insight" || "medicine" || "perception" || "survival":
+      result.append("Use Wisdom");
+      break;
+    case "decept" || "intim" || "perform" || "persuade":
+      result.append("Use Charisma");
+      break;
+    case "athletics":
+      result.append("Use Strength");
+      break;
+    default:
+      result.append("Use ...");
+  }
+}
+
+
+const rollHelp = () => {
   var selection = document.getElementById('rolls').value;
   var result = document.getElementById('roll-p');
   result.innerHTML = "";
