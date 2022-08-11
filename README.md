8e6200b9f1ce344946a604101629e757d69bd249
Tobias Wallace <twallace@catalyte.io>
2022-08-11 15:20:45 -0500

    Add in-code documentation

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # dnd-app
-This is a simple website designed to help my friends and I play Dungeons and Dragons. As such, it's tailored
-to very specific recurring questions we encounter while
-playing.
+This is a simple website designed to help my friends and I play Dungeons and Dragons. As such, it's tailored to very specific recurring questions we encounter while playing.
 
 This uses simple HTML, CSS, and JavaScript so that the 
 website can be deployed on Neocities. You can see the live version of the application [here](link.com). The website is built with mobile functionality in mind, but can be used on a tablet or desktop without issue.
@@ -12,23 +10,24 @@ This is a simple website, so simply opening the `index.html` will allow you to b
 ### Classes
 Since this is tailored for my personal game, the logic for displaying player information is built around the character's names- NOT classes. It's also worth noting that I have used `_class` in code instead of `class` - class is a reserved keyword and can't be used as a variable in JavaScript. Unfortunately there wasn't another logical word choice I could think of, so the _ remains.
 
-While the spellcaster classes are visible in code (as needed for the spellcasterHelp function), the other character's don't have visible classes in the code. Amy is a Monk and Ishmael (id='izzy') is a Barbarian. Karrde is a homebrew class called Reaper, more information about that class can be found [here](link.com).
+Karrde is a homebrew class called Reaper, more information about that class can be found [here](link.com).
 
 If you want to tailor this app for your own game, or for a more general audience the following will need to be changed:
-- 'character' Select on `line 56` of index.html   
+- 'character' Select on `line 65` of index.html   
     The select list only includes character names, this can be edited to reflect other players in your game or all classes
-- characterHelp() on `line 47` of app.js
+- characterHelp() on `line 67` of app.js
 
-    This is the section that will need the most editing. On `line 66` there's a switch statement for selecting different characters. This can be set up for your own characters or all classes. There is no default on my switch statement, depending on the edits you make you want to ensure you have one.
+    This is the section that will need the most editing. On `line 86` there's a switch statement for selecting different characters. This can be set up for your own characters or all classes. There is no default on my switch statement, depending on the edits you make you want to ensure you have one.
 
-    The helper method spellcasterHelp() on `line 83` won't need to be edited and if you're editing this so all classes can be searched, you can change the type parameter from a string to the character variable. 
+    The helper method spellcasterHelp() on `line 207` only has functionality for artificers, clerics, and reapers. If you want to implement this for more than 3 types of spellcasters, you may want to change the if/else statement for a switch statement.
+
 - 'level' Select on `line 67` of index.html   
     To prevent players in my game from getting confused, only the player's current level and 2 more are shown. The logic in app.js can handle proficiency bonsuses for all 20 levels, just the front-end is limited.
 
 ## License
 MIT License
 
-Copyright (c) 2022 Tobias Wallace
+Copyright (c) 2022 Tobias Valentine
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/**
+ * Allows user to learn which skill the ability is related to
+ * @returns which skill the ability uses
+ */
 const skillsHelp = () => {
   var selection = document.getElementById('skills').value;
   var result = document.getElementById('skill-p');
@@ -36,7 +40,10 @@ const skillsHelp = () => {
   }
 }
 
-
+/**
+ * Allows the user to ask which dice they should roll for a set of specific scenarios
+ * @returns what dice the user should roll
+ */
 const rollHelp = () => {
   var selection = document.getElementById('rolls').value;
   var result = document.getElementById('roll-p');
@@ -53,6 +60,10 @@ const rollHelp = () => {
   }
 }
 
+/**
+ * Takes user input and runs the correct Helper function for the character's class.
+ * Also calculates the character's proficiency bonus
+ */
 const characterHelp = () => {
   var character = document.getElementById('character').value;
   var level = document.getElementById('level').value;
@@ -74,7 +85,7 @@ const characterHelp = () => {
   // provide character info
   switch (character) {
     case "amy":
-      monkHelper(result, level, prof, 3);
+      monkHelper(result, prof, level, 3);
       break;
     case "clyde":
       spellcasterHelper(result, prof, level, 0, 'cleric');
@@ -91,8 +102,16 @@ const characterHelp = () => {
   }
 }
 
-const monkHelper = (div, level, proficiency, modifier) => {
-  // martial arts dice
+/**
+ * Calculates and displays spell information for the Monk class
+ * @param {*} div where the information will return on the webpage
+ * @param {*} level character's current level
+ * @param {*} modifier character's Dexterity modifier 
+ * @param {*} proficiency character's proficiency bonus as calculated in characterHelp()
+ * @returns Martial Arts Di, Total Ki Points, and Ki Save DC
+ */
+const monkHelper = (div, level, modifier, proficiency) => {
+  // Calculates Martial Arts Di
   var dice = "1d4";
   if (level > 4 && level < 11) {
     dice = "1d6"; 
@@ -103,27 +122,39 @@ const monkHelper = (div, level, proficiency, modifier) => {
     dice = "1d10"; 
   }
   
-  var martialTitle = div.appendChild(document.createElement('h4'));
+  var div1 = div.appendChild(document.createElement('div'));
+  var martialTitle = div1.appendChild(document.createElement('p'));
+  martialTitle.classList.add("bolder");
   martialTitle.append("Martial Arts Die:");
-  var martialTxt = div.appendChild(document.createElement('p'));
+  var martialTxt = div1.appendChild(document.createElement('p'));
   martialTxt.append(dice);
   
-  // Ki Points
-  var kiPointsTitle = div.appendChild(document.createElement('h4'));
+  // Calculates Ki Points
+  var div2 = div.appendChild(document.createElement('div'));
+  var kiPointsTitle = div2.appendChild(document.createElement('p'));
+  kiPointsTitle.classList.add("bolder");
   kiPointsTitle.append("Total Ki Points:");
-  var kiPointsTxt = div.appendChild(document.createElement('p'));
+  var kiPointsTxt = div2.appendChild(document.createElement('p'));
   kiPointsTxt.append(`${level}`);
   
-  // Ki Save DC
-  var kiSaveTitle = div.appendChild(document.createElement('h4'));
+  // Calculates Ki Save DC
+  var div3 = div.appendChild(document.createElement('div'));
+  var kiSaveTitle = div3.appendChild(document.createElement('p'));
+  kiSaveTitle.classList.add("bolder");
   kiSaveTitle.append("Ki Save DC:");
-  var kiSaveTxt = div.appendChild(document.createElement('p'));
+  var kiSaveTxt = div3.appendChild(document.createElement('p'));
   kiSaveTxt.append(`${8 + proficiency} + Wisdom modifier  (+${modifier}) = ${8 + proficiency + modifier}`);
         
 }
 
+/**
+ * Calculates and displays spell information for the Barbarian class
+ * @param {*} div where the information will return on the webpage
+ * @param {*} level character's current level
+ * @returns # of Rages and Rage damage bonus
+ */
 const barbarianHelper = (div, level) => {
-  // # of rages
+  // Calculates # of Rages
   var rages = 2;
   if (level > 2 && level < 6) {
     rages = 3; 
@@ -140,12 +171,15 @@ const barbarianHelper = (div, level) => {
   else {
     rages = "unlimited";
   }
-  var rageAmtTitle = div.appendChild(document.createElement('h4'));
+
+  var div1 = div.appendChild(document.createElement('div'));
+  var rageAmtTitle = div1.appendChild(document.createElement('p'));
+  rageAmtTitle.classList.add("bolder");
   rageAmtTitle.append("Number of Rages:");
-  var rageAmtTxt = div.appendChild(document.createElement('p'));
+  var rageAmtTxt = div1.appendChild(document.createElement('p'));
   rageAmtTxt.append(`${rages}`);
 
-  // rage damage
+  // Calculates Rage Damage
   var damage = "+2";
   if (level > 8 && level < 16) {
     damage = "+3";
@@ -153,12 +187,23 @@ const barbarianHelper = (div, level) => {
   else if (level >= 16) {
     damage = "+4";
   }
-  var rageDmgTitle = div.appendChild(document.createElement('h4'));
+  var div2 = div.appendChild(document.createElement('div'));
+  var rageDmgTitle = div2.appendChild(document.createElement('p'));
+  rageDmgTitle.classList.add("bolder");
   rageDmgTitle.append("Rage Damage:");
-  var rageDmgTxt = div.appendChild(document.createElement('p'));
+  var rageDmgTxt = div2.appendChild(document.createElement('p'));
   rageDmgTxt.append(`${damage}`);
 }
 
+/**
+ * Calculates and displays spell information for full spellcaster classes
+ * @param {*} div where the information will return on the webpage
+ * @param {*} proficiency character's proficiency bonus as calculated in characterHelp()
+ * @param {*} level character's current level
+ * @param {*} modifier character's spellcasting modifier 
+ * @param {*} _class character's combat class
+ * @returns Spellcasting ability, Spellcasting Modifier, Spell Save, and Spells Known
+ */
 const spellcasterHelper = (div, proficiency, level, modifier, _class) => {
   // Sets spellcasting ability and spellsave
   if (_class == 'cleric') {
@@ -167,18 +212,18 @@ const spellcasterHelper = (div, proficiency, level, modifier, _class) => {
   }
   else if (_class == 'reaper') {
     var ability = `WHAT (+${modifier})`;
-    var spellsave = `${ability} + ${level}`;
+    var spellsave = `${ability} + ${level} = ${modifier + parseInt(level)}`;
   }
   else if (_class == 'artificer') {
     var ability = `Intelligence (+${modifier})`;
-    var spellsave = `${ability} + ${Math.floor(level/2)}`;
+    var spellsave = `${ability} + ${Math.floor(level/2)} = ${modifier + Math.floor(level/2)}`;
   }
 
   // Spell Ability
   var div1 = div.appendChild(document.createElement('div'));
   var abilitytitle = div1.appendChild(document.createElement('p'));
   abilitytitle.classList.add("bolder");
-  abilitytitle.append("Spell Ability: ");
+  abilitytitle.append("Spellcasting Ability: ");
   var abilitytxt = div1.appendChild(document.createElement('p'));
   abilitytxt.append(`${ability}`);
 
@@ -186,9 +231,9 @@ const spellcasterHelper = (div, proficiency, level, modifier, _class) => {
   var div2 = div.appendChild(document.createElement('div'));
   var modifiertitle = div2.appendChild(document.createElement('p'));
   modifiertitle.classList.add("bolder");
-  modifiertitle.append("Spell Modifier:");
+  modifiertitle.append("Spellcasting Modifier:");
   var modifiertxt = div2.appendChild(document.createElement('p'));
-  modifiertxt.append(`${ability} + ${proficiency} = ${proficiency + modifier}`);
+  modifiertxt.append(`${ability} + ${proficiency} = +${proficiency + modifier}`);
 
   // Spell Save
   var div3 = div.appendChild(document.createElement('div'));
